26cd11914bb600db1e576c1629ae5ec6ea8c4f22
cristianRodri <an1me@hotmail.es>
2023-01-19 12:38:36 -0500

    02-video-code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { throttle } from 'throttle-debounce';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const eventCounter = {
+  duration: 61.857,
+  percent: 0.049,
+  seconds: 3.034,
+};
+
+player.on('play', () => {
+  console.log((eventCounter.duration = +1));
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
